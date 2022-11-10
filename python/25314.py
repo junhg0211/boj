21ec99f7cf545a291bb9f9bf66bb31033d5e77e7
Sch <junhg0211@gmail.com>
2022-11-10 09:29:27 +0900

    solved 2 questions

---
+++ python/25314.py
@@ -0,0 +1,6 @@
+def main():
+    print(' '.join('long' for _ in range(int(input()) // 4)) + ' int')
+
+
+if __name__ == '__main__':
+    main()
---
+++ python/25372.py
@@ -0,0 +1,7 @@
+def main():
+    for _ in range(int(input())):
+        print('yes' if 6 <= len(input()) <= 9 else 'no')
+
+
+if __name__ == '__main__':
+    main()
