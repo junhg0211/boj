f92f170174e43fdfa9f774516835bf3f7811e6fc
Sch <junhg0211@gmail.com>
2022-11-17 19:45:02 +0900

    solved a question

--- python/1340.py
+++ python/1340.py
@@ -5,7 +5,7 @@ days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 def parse_datetime(string: str):
     month, day, year, time = string.split()
     month = names.index(month)
-    day = int(day[:-1])
+    day = int(day[:-1]) - 1
     year = int(year)
     hour, minute = map(int, time.split(':'))
     return year, month, day, hour, minute
@@ -13,9 +13,9 @@ def parse_datetime(string: str):
 
 def main():
     year, month, day, hour, minute = parse_datetime(input())
-    if year % 4 == 0 and year % 100 != 0 or year % 400 == 1:
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
         days[1] = 29
-    print((sum(days[:month]) + (day - 1) + (hour + (minute/60))/24) / sum(days) * 100)
+    print((sum(days[:month]) + day + (hour + (minute/60)) / 24) / sum(days) * 100)
 
 
 if __name__ == '__main__':
