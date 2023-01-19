1c294c9cac142e2a2c71ceb1270d228ddf415550
Sch <junhg0211@gmail.com>
2023-01-19 22:38:15 +0900

    solve 2941

---
+++ python/1/2941.py
@@ -0,0 +1,15 @@
+def main():
+    print(len(input()
+        .replace('c=', '!')
+        .replace('c-', '!')
+        .replace('dz=', '!')
+        .replace('d-', '!')
+        .replace('lj', '!')
+        .replace('nj', '!')
+        .replace('s=', '!')
+        .replace('z=', '!')
+    ))
+
+
+if __name__ == '__main__':
+    main()
