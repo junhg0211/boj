0327892093aaad7635a581d758aab336048d96a7
junhg0211 <junhg0211@gmail.com>
2023-02-17 13:54:33 +0900

    solve 27328

---
+++ python/27328.py
@@ -0,0 +1,13 @@
+def main():
+    a, b = int(input()), int(input())
+
+    if a < b:
+        print('-1')
+    elif a == b:
+        print('0')
+    else:
+        print('1')
+
+
+if __name__ == '__main__':
+    main()
