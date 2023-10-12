95dded1d3b236231196eb58dc8166af390889c34
Sch <junhg0211@gmail.com>
2023-10-13 00:08:10 +0900

    solve 10951

---
+++ cpp/1/10951.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    while (true) {
+        int a, b;
+        std::cin >> a >> b;
+
+        if (std::cin.eof()) {
+            break;
+        }
+
+        std::cout << a + b << "\n";
+    }
+
+    return 0;
+}
