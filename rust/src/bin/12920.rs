fe5259259b4abc1120324123ebd9187380d24c43
Sch <junhg0211@gmail.com>
2023-10-16 11:33:30 +0900

    try 12920

---
+++ rust/src/bin/12920.rs
@@ -0,0 +1,47 @@
+use std::io::stdin;
+use std::cmp::max;
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let mut iter = input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse::<usize>().unwrap());
+    let thing_count = iter.next().unwrap();
+    let max_weight = iter.next().unwrap();
+    let mut real_thing_count = 0;
+
+    let mut things = Vec::new();
+    for _ in 0..thing_count {
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+        let mut iter = input
+            .trim()
+            .split_whitespace()
+            .map(|x| x.parse::<usize>().unwrap());
+        let weight = iter.next().unwrap();
+        let value = iter.next().unwrap();
+        let count = iter.next().unwrap();
+        real_thing_count += count;
+
+        for _ in 0..count {
+            things.push((weight, value));
+        }
+    }
+
+    let mut dp = vec![vec![0; max_weight+1]; real_thing_count+1];
+    for i in 1..=real_thing_count {
+        for j in 1..=max_weight {
+            let (weight, value) = things[i-1];
+
+            if weight > j {
+                dp[i][j] = dp[i-1][j];
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value);
+            }
+        }
+    }
+
+    println!("{}", dp[real_thing_count][max_weight]);
+}
