ec7691ca649054fe5b4ef7770860bf5a4afca9c1
Sch <junhg0211@gmail.com>
2023-10-29 09:17:28 +0900

    solve 9063

---
+++ rust/src/3/9063.rs
@@ -0,0 +1,28 @@
+use std::io::stdin;
+use std::cmp::{ min, max };
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let count = input.trim().parse::<usize>().unwrap();
+
+    let (mut min_x, mut max_x) = (i32::MAX, i32::MIN);
+    let (mut min_y, mut max_y) = (i32::MAX, i32::MIN);
+    for _ in 0..count {
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+        let mut iter = input
+            .trim()
+            .split_whitespace()
+            .map(|x| x.parse::<i32>().unwrap());
+        let x = iter.next().unwrap();
+        let y = iter.next().unwrap();
+
+        min_x = min(min_x, x);
+        max_x = max(max_x, x);
+        min_y = min(min_y, y);
+        max_y = max(max_y, y);
+    }
+
+    println!("{}", (max_x - min_x) * (max_y - min_y));
+}
