6000b26ec402b0cd9c591758cb157a015c041a07
Sch <junhg0211@gmail.com>
2024-03-23 21:08:01 +0900

    commit 18108

---
+++ c/8/18108.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // get year
+    int year;
+    scanf("%d", &year);
+
+    // print result
+    printf("%d\n", year - 543);
+
+    // finalise
+    return 0;
+}
