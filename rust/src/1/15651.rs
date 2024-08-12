12f0980891f0d5442c6bf7d4552386d66baa3636
Sch <junhg0211@gmail.com>
2024-08-12 13:41:27 +0900

    solve 15651

---
+++ rust/src/1/15651.rs
@@ -0,0 +1,32 @@
+use std::io::{stdin, stdout, BufWriter, Stdout, Write};
+
+fn backtrack(number: u32, count: u32, numbers: &mut Vec<u32>, writer: &mut BufWriter<Stdout>) {
+    for i in 1..=number {
+        numbers.push(i);
+
+        if count == 1 {
+            for &n in numbers.iter() {
+                write!(writer, "{} ", n).unwrap();
+            }
+            writeln!(writer).unwrap();
+        } else {
+            backtrack(number, count - 1, numbers, writer);
+        }
+
+        numbers.pop().unwrap();
+    }
+}
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let mut iter = input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse::<u32>().unwrap());
+    let number = iter.next().unwrap();
+    let count = iter.next().unwrap();
+
+    let mut writer = BufWriter::new(stdout());
+    backtrack(number, count, &mut Vec::new(), &mut writer);
+}
